43f6b3d3691a569c87cbcba167554fba9fdc4a70
Jerry-Hong <s6323859@gmail.com>
2016-07-17 01:51:17 +0800

    Fix .ts file problem #1

--- src/extension.ts
+++ src/extension.ts
@@ -7,10 +7,13 @@ import { resolve, sep } from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
     var completeProvider = new CompleteProvider();
+    vscode.languages.getLanguages().then(data => console.log(data))
     var disposable = vscode.languages.registerCompletionItemProvider('*', 
     completeProvider, '"', '\'', '/');
 	context.subscriptions.push(disposable);
 }
+
+
 /**
  *  CompleteProvider
  */
@@ -18,21 +21,20 @@ class CompleteProvider implements vscode.CompletionItemProvider{
     provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): Thenable<vscode.CompletionItem[]>{
         const currentPath = getCurrentPath(document.fileName);
         const lineAt = document.lineAt(position);
-        const lineText = document.getText(lineAt.range);     
+        const lineText = document.getText(lineAt.range);   
         let userKeyInStr = getUserKeyIn(lineText, position.character);
         let finalPath = resolve(currentPath, userKeyInStr);
         return new Promise((resolve, reject) => {
-            let items: vscode.CompletionItem[];
             readdir(finalPath, (err, data) => {
+                console.log(data);
                 if(err){
-                    resolve([]);                    
+                    reject();                    
                 } else {
-                    items = data.filter(item => {
-                    return item[0] === '.' ? false : true;
-                    }).map(item => {
-                        return new vscode.CompletionItem(item);
-                    });
-                    resolve(items);
+                    data.unshift(''); // hack .ts file, first one is empty string and it will be correct when user typing dot 
+                    resolve(data
+                        .filter(item => item[0] !== '.')
+                        .map(item => new vscode.CompletionItem(item, vscode.CompletionItemKind.File))
+                    );
                 }
             });
         });
